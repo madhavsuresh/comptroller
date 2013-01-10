b2fefde2123950196011d49453369c7ae883a978
Madhav Suresh <madhav.a.suresh@gmail.com>
2013-01-10 11:58:37 -0600

    form works, stores to DB

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
-from app import Base
-from sqlalchemy import Column,Integer,DateTime,String,Text
+from app.database import Base
+from sqlalchemy import Column,Integer,String,Text
 import constants
 
 
 class Presenters(Base):
+    __tablename__ = 'presenters'
     id = Column(Integer,primary_key=True)
+    reg_num = Column(String(constants.MAX_REGNUM),unique=True)
     name = Column(String(constants.MAX_NAME))
     email = Column(String(constants.MAX_EMAIL))
     institution = Column(String(constants.MAX_INSTITUTION))
@@ -13,4 +15,29 @@ class Presenters(Base):
     title = Column(Integer(constants.MAX_TITLE))
     discipline = Column(String())
     abstract = Column(Text())
+
+    def __init__(self,name=None,email=None,
+                institution=None,major=None,year=None,title=None,
+                abstract_title=None, discipline=None,abstract=None,
+                arg_dict=None):
+
+        if arg_dict:
+            self.name = arg_dict['name']
+            self.email = arg_dict['email']
+            self.institution = arg_dict['institution']
+            self.major = arg_dict['major']
+            self.year = arg_dict['year']
+            self.abstract_title = arg_dict['abstract_title']
+            self.discipline = arg_dict['discipline']
+            self.abstract = arg_dict['abstract']
+        else:
+            self.name = name
+            self.email = email
+            self.institution = institution
+            self.major = major
+            self.year = year
+            self.abstract_title = abstract_title
+            self.discipline = discipline
+            self.abstract = abstract
+
 
--- app/views.py
+++ app/views.py
@@ -1,26 +1,19 @@
 from flask import render_template, flash, redirect
 from app import app 
+from app.database import db_session
 from forms import LoginForm,RegistrationForm
+from app.models import Presenters
+
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
 
 @app.route('/')
 @app.route('/index')
 def index():
+    return 'caurs!!'
 
-    user = { 'nickname': 'Miguel' } # fake user
-    posts = [ # fake array of posts
-            { 
-                'author': { 'nickname': 'John' }, 
-                'body': 'Beautiful day in Portland!' 
-                },
-            { 
-                'author': { 'nickname': 'Susan' }, 
-                'body': 'The Avengers movie was so cool!' 
-                }
-            ]
-    return render_template('index.html',
-                   title = 'Home',
-                   user = user,
-                   posts = posts)
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
@@ -32,12 +25,28 @@ def login():
             form = form,
             providers = app.config['OPENID_PROVIDERS'])
 
+def form_to_dict(form):
+    ret = {}
+    ret['name'] = form.name.data
+    ret['email'] = form.email.data
+    ret['institution'] = form.institution.data
+    ret['major'] = form.major.data
+    ret['year'] = form.year.data
+    ret['abstract_title'] = form.abstract_title.data
+    ret['discipline'] = form.discipline.data
+    ret['abstract'] = form.abstract.data
+    return ret
+
+def handle_form_data(register_dict):
+    p = Presenters(arg_dict=register_dict)
+    db_session.add(p)
+    db_session.commit()
+
 @app.route('/register',methods=['GET','POST'])
 def register():
     form = RegistrationForm()
-    print 'hello world'
     if form.validate_on_submit():
-        print 'hello world'
+        handle_form_data(form_to_dict(form))
         flash('stuff' + form.name.data)
         return redirect('/index')
     return render_template('register.html',
