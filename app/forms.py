5afbe815a45cd07944c4411d28f26538f59b4032
Madhav Suresh <madhav.a.suresh@gmail.com>
2013-01-18 00:23:41 -0600

    example for forms with biology subfields

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask.ext.wtf import Form, TextField, BooleanField,SelectField,validators,TextAreaField,IntegerField,html5
+from flask.ext.wtf import Form, TextField, BooleanField,SelectField,validators,TextAreaField,IntegerField,html5,RadioField
 import constants
 
 class LoginForm(Form):
@@ -8,9 +8,17 @@ class LoginForm(Form):
 schools = [('depaul','Depaul University'),('iit','Illinois Institute of Technology'),
         ('loyola','Loyola University Chicago'),('northwestern','Northwestern University'),
         ('uofc','University of Chicago'),('uic','University of Illinois at Chicago'),('other','Other')]
+bio_sub = [('biochem_biophys','Biochemistry and Biophysics'),
+        ('clinre','Clinical Research'),
+                    ('cellgendev','Cellular/Molecular Biology, Genetics, and Developmental Biology'),
+                    ('immmicroviro','Immunology/Microbiology/Virology'),
+                    ('neuro','Neurosciences'),
+                    ('compubio','Systems Biology/Computational Biology')]
+
 
 class RegistrationForm(Form):
-    name = TextField('Name', [validators.length(max=constants.MAX_NAME),validators.required()])
+    fname = TextField('First Name', [validators.length(max=constants.MAX_NAME),validators.required()])
+    lname = TextField('Last Name', [validators.length(max=constants.MAX_NAME),validators.required()])
     email = TextField('Email',[validators.length(max=constants.MAX_EMAIL),validators.required(),validators.email()])
     confirm_email = TextField('Confirm Email',[
                                                 validators.required(), 
@@ -23,4 +31,5 @@ class RegistrationForm(Form):
     abstract_title = TextField('Research Project Title',[validators.required(),validators.length(max=constants.MAX_TITLE)])
     discipline = TextField('Academic Discipline',[validators.required()])
     abstract = TextAreaField('Abstract',[validators.required()])
+    bio_subfield = RadioField('Biological Science Subfield',choices= bio_sub)
 
